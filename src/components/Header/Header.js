ea69e9fb416ef4b1b8fc26c606e2f79e5f6dcfca
Светличный Александр Викторович <asvetly1337@gmail.com>
2022-02-07 19:31:15 +0300

    Exercise 2: Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -27,11 +27,20 @@ const Header = () => {
 };
 
 const MainHeader = styled.div`
-  padding: 0 32px;
+  padding: 15px 32px;
   border-bottom: 1px solid ${COLORS.gray[300]};
+  display: flex;
+  align-items: baseline;
+  
+  & > :first-child {
+    flex-basis: 300px;
+  }
 `;
 
-const Nav = styled.nav``;
+const Nav = styled.nav`
+  display: flex;
+  gap: 30px;
+`;
 
 const NavLink = styled.a`
   font-size: 1.125rem;
